867ef03dc67f1c0a06121bb42ecc462d7fba266c
Shoaib Uzair <71070294+ShoaibUzair@users.noreply.github.com>
2021-05-24 21:47:36 +0500

    Rate Product Front End
    According to oneeb the logic I had one flaw that we could not show the user the rating that they did, so for that we have to use a map and some other complicated function.

--- clickandpick/lib/BuyerDashboard/myorders.dart
+++ clickandpick/lib/BuyerDashboard/myorders.dart
@@ -1,12 +1,12 @@
 import 'package:ClickandPick/BuyerDashboard/Buyer_Drawer.dart';
 import 'package:ClickandPick/BuyerDashboard/title_text.dart';
-import 'package:ClickandPick/Manager/Riders.dart';
-import 'package:ClickandPick/SellerDashboard/data.dart';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 import 'light_color.dart';
 
@@ -60,70 +60,109 @@ class _MyordersState extends State<Myorders> {
                   itemBuilder: (context, index) {
                     DocumentSnapshot ds = snapshot.data.docs[index];
                     return Column(children: <Widget>[
-                      Container(
-                        height: 80,
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Row(children: <Widget>[
-                            AspectRatio(
-                                aspectRatio: 1.2,
-                                child: Stack(children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 18.0),
-                                    child: Container(
-                                        height: 100,
-                                        width: 100,
-                                        child: Stack(children: <Widget>[
-                                          Align(
-                                            alignment: Alignment.bottomLeft,
-                                            child: Container(
-                                              decoration: BoxDecoration(
-                                                  color: LightColor.lightGrey,
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          10)),
-                                            ),
-                                          ),
-                                          CachedNetworkImage(
-                                              imageUrl: ds['image']),
-                                        ])),
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {
+                            return showDialog(
+                              context: context,
+                              builder: (_) => AlertDialog(
+                                title: Text('Rate the Product'),
+                                content: SmoothStarRating(
+                                  allowHalfRating: false,
+                                  size: 30.0,
+                                  color: Colors.yellow,
+                                  //rating: widget.data.rating,
+                                  // onRated: (double value) async {
+                                  //debugPrint(
+                                  //'Image no. $index was rated $value stars!!!');
+                                ),
+                                actions: [
+                                  TextButton(
+                                    child: Text('Yes'),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                      //signOut();
+                                    },
                                   ),
-                                ])),
-                            Expanded(
-                                child: ListTile(
-                                    title: TitleText(
-                                      text: ds['name'].toString(),
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                                    subtitle: Row(
-                                      children: <Widget>[
-                                        TitleText(
-                                          text: '\RS ',
-                                          color: LightColor.red,
-                                          fontSize: 12,
-                                        ),
-                                        TitleText(
-                                          text: ds['total'].toString(),
-                                          fontSize: 14,
-                                        ),
-                                      ],
-                                    ),
-                                    trailing: Container(
-                                      width: 35,
-                                      height: 35,
-                                      alignment: Alignment.center,
-                                      decoration: BoxDecoration(
-                                          color: LightColor.lightGrey
-                                              .withAlpha(150),
-                                          borderRadius:
-                                              BorderRadius.circular(10)),
-                                      child: TitleText(
-                                        text: ds['quantity'].toString(),
-                                        fontSize: 12,
+                                  TextButton(
+                                    child: Text('No'),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                          child: Ink(
+                            height: 80,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Row(children: <Widget>[
+                                AspectRatio(
+                                    aspectRatio: 1.2,
+                                    child: Stack(children: <Widget>[
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 18.0),
+                                        child: Container(
+                                            height: 100,
+                                            width: 100,
+                                            child: Stack(children: <Widget>[
+                                              Align(
+                                                alignment: Alignment.bottomLeft,
+                                                child: Container(
+                                                  decoration: BoxDecoration(
+                                                      color:
+                                                          LightColor.lightGrey,
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              10)),
+                                                ),
+                                              ),
+                                              CachedNetworkImage(
+                                                  imageUrl: ds['image']),
+                                            ])),
                                       ),
-                                    ))),
-                          ]),
+                                    ])),
+                                Expanded(
+                                    child: ListTile(
+                                        title: TitleText(
+                                          text: ds['name'].toString(),
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                        subtitle: Row(
+                                          children: <Widget>[
+                                            TitleText(
+                                              text: '\RS ',
+                                              color: LightColor.red,
+                                              fontSize: 12,
+                                            ),
+                                            TitleText(
+                                              text: ds['total'].toString(),
+                                              fontSize: 14,
+                                            ),
+                                          ],
+                                        ),
+                                        trailing: Container(
+                                          width: 35,
+                                          height: 35,
+                                          alignment: Alignment.center,
+                                          decoration: BoxDecoration(
+                                              color: LightColor.lightGrey
+                                                  .withAlpha(150),
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: TitleText(
+                                            text: ds['quantity'].toString(),
+                                            fontSize: 12,
+                                          ),
+                                        ))),
+                              ]),
+                            ),
+                          ),
                         ),
                       ),
                       Divider(
